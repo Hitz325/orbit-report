3d4d52900073d292dbd91c739ba045ffabe5d280
unknown <thitschler88@gmail.com>
2021-04-05 17:57:35 -0500

    Completed Part 6

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -14,4 +14,19 @@ export class Satellite
         this.orbitType = orbitType;
         this.operational = operational;
     }
+
+    shouldShowWarning(): boolean 
+    {
+        if(this.type.toLowerCase() === "space debris")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    
 }
